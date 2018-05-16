d7ebfee7c430ef72f441ba780ebefa970b15c5d0
latilanoh@gmail.com <latilanoh@gmail.com>
2018-05-16 13:59:42 -0500

    Se sube la versión inicial del 0.0.2 con sus correspondientes cambios, estilo y codigo en js y css

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var task = document.getElementById('textInitial');
+var saveTasks = document.getElementById('inputSave');
 
 task.addEventListener('click', function() { 
     var hideFirstText = document.getElementById('textInitial');
@@ -6,6 +7,34 @@ task.addEventListener('click', function() {
     var showForm = document.getElementById('tablero-lista');
     showForm.style.display = 'block';
 });
+
+saveTasks.addEventListener('click', function() {
+    var container = document.getElementById('containerList');
+    var littleContainer = document.createElement('div');
+    var addNewA = document.createElement('a');
+    addNewA.innerHTML = "Añadir una tarea...";
+
+    var textSave = document.getElementById('inputText').value;
+    document.getElementById('inputText').value = '';
+    var commentSave = document.createTextNode(textSave);
+    var titleList = document.createElement('h3');
+    titleList.classList.add('titleList');
+    var insertInput = document.createElement('input');
+    insertInput.type = "button";
+    insertInput.setAttribute('class', 'new-Input');
+    insertInput.value = '';
+    
+    // Anidación de elementos
+    titleList.appendChild(commentSave);
+    littleContainer.appendChild(titleList);
+    littleContainer.appendChild(insertInput);
+    container.appendChild(littleContainer);
+    container.appendChild(addNewA);
+});
+
+/*
+var inputText = document.getElementById('inputText').value;
+console.log(inputText); */
 
 /*
 function createList = {
